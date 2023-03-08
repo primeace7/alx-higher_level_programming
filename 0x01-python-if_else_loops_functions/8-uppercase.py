32a0f41472c47a7196aa88157cef52e63ac911c1
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-08 09:05:15 +0000

    Fix task 8 test error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     if str == '':
-        str = ' '
+        str = '\n'
     for i in str:
         if (ord(i) >= 97 and ord(i) <= 122):
             if i == str[-1]:
@@ -10,9 +10,8 @@ def uppercase(str):
                 use = ''
             print('{}'.format(chr(ord(i) - 32)), end=use)
         else:
-            if i == str[-1]:
+            if i == str[-1] and str != '\n':
                 use = '\n'
             else:
                 use = ''
             print('{}'.format(i), end=use)
-
