7e7f8f6ac4f2184b1920362c2ada9a9fb24f466e
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-09 15:21:58 +0000

    Fix task 4 test error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+names = sorted(dir('hidden_d'))
 if __name__ == '__main__':
-    names = sorted(dir('hidden_4.pyc'))
     for name in names:
-        if not name.startswith('_'):
+        if not name.startswith('__'):
             print('{}'.format(name))
