05dd683909b6e8bbbf4c8dc8f0155627cffac105
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-09 15:40:07 +0000

    Add task 100 python script

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print(f'Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    ops = ['+', '-', '*', '/']
+    if argv[2] not in ops:
+        print(f'Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+
+    print(f'{num1} {argv[2]} {num2} = ', end='', sep='')
+    if argv[2] == ops[0]:
+        print(f'{num1 + num2}')
+    elif argv[2] == ops[1]:
+        print(f'{num1 - num2}')
+    elif argv[2] == ops[2]:
+        print(f'{num1 * num2}')
+    elif argv[2] == ops[3]:
+        print(f'{num1 / num2}')
