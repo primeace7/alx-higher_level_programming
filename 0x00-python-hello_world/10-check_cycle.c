f8afdcf22f169e9e6d68dba7851399d376d0ac0b
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-10 14:03:00 +0000

    Fix task 10 linked list errors

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,30 +1,6 @@
 #include "lists.h"
 
 /**
- * check_list - iterate through a list and check for a cycle
- * @head: pointer to the first node in the list
- * @list: pointer to the current node in the list
- * @i: the index in the list where @list currently points to
- * Return: 1 if the list has a cycle, 0 otherwise
- */
-
-int check_list(listint_t *head, listint_t *list, int i)
-{
-	listint_t *current_node;
-	int counter;
-
-	current_node = head;
-
-	for (counter = 0; counter < i; counter++)
-	{
-		if (current_node == list)
-			return (1);
-		current_node = current_node->next;
-	}
-	return (0);
-}
-
-/**
  * check_cycle - check if a linked list is circular or not
  * @list: pointer to the head node of the list
  * Return: 1, if the list has a cycle, or 0 otherwise
@@ -32,8 +8,8 @@ int check_list(listint_t *head, listint_t *list, int i)
 
 int check_cycle(listint_t *list)
 {
-	listint_t *head;
-	int i = 0;
+	listint_t *head, *curr;
+	int c, i = 0;
 
 	head = list;
 
@@ -48,7 +24,8 @@ int check_cycle(listint_t *list)
 		list = list->next;
 		i++;
 
-		if (check_list(head, list, i))
+		curr = head;
+		for (c = 0; curr == list && c < i; c++, curr = curr->next)
 			return (1);
 	}
 }
