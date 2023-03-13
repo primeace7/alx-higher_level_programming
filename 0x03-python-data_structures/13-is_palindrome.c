87862869037bcec528b02d64c89b6bb0ecbc4cdd
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-13 19:07:51 +0000

    Rewrite task 13 to fix test errors

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * list_index - get the data at an index of a linked list
+ * @list: pointer to the first node in the list
+ * @n: the index to get
+ * Return: the data at index n
+ */
+
+int list_index(listint_t *list, int n)
+{
+	int i;
+	if (n = 0)
+		return (list->n);
+	for (i = 0; i < n; i++)
+		list = list->next;
+	return (list->n);
+}
+
+/**
  * is_palindrome - check if a singly linked list is a palindrome
  * @head: pointer to the head pointer of the list to check
  * Return: 1 if the list is palindromic, 0 if it isn't
@@ -8,29 +25,21 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *iter;
-	int i, j, counter, midpoint;
+	listint_t *list;
+	int i, list_len, midpoint;
 
-	if ((*head)->next == NULL)
-		return (0);
-	if (*head == NULL)
-		return (1);
+	list = *head;
+	for (list_len = 1; list->next != NULL; list_len++)
+		list = list->next;
 
-	iter = *head;
-	for (counter = 1; iter->next != NULL; counter++)
-		iter = iter->next;
-
-	midpoint = counter / 2;
-	counter -= 2;
+	midpoint = list_len / 2;
+	list = *head;
 
 	for (i = 0; i < midpoint; i++)
 	{
-		if ((*head)->n != iter->n)
+		if (list != list_index(*head, list_len - i -1))
 			return (0);
-		for (iter = *head, j = 0; j < counter; j++)
-			iter = iter->next;
-		counter -= 2;
-		*head = (*head)->next;
+		list = list->next;
 	}
 	return (1);
 }
