7ea46895d0d7afe4524ae7072d502e245badcab7
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-13 16:35:35 +0000

    Add function description for task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,12 @@
 #include "object.h"
 #include "listobject.h"
 
+/**
+ * print_python_list_info - print some info about a python list using C
+ * @p: pointer to a PyObject, such as a list
+ * Return: nothing
+ */
+
 void print_python_list_info(PyObject *p)
 {
 	return (0);
