53f351c50a21185b2cd632286838548feb118da6
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-13 19:35:56 +0000

    Fix task 13 test errors

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * list_index - get the data at an index of a linked list
- * @list: pointer to the first node in the list
- * @n: the index to get
- * Return: the data at index n
- */
-
-int list_index(listint_t *list, int n)
-{
-	int i;
-	if (n == 0)
-		return (list->n);
-	for (i = 0; i < n; i++)
-		list = list->next;
-	return (list->n);
-}
-
-/**
  * is_palindrome - check if a singly linked list is a palindrome
  * @head: pointer to the head pointer of the list to check
  * Return: 1 if the list is palindromic, 0 if it isn't
@@ -33,13 +16,13 @@ int is_palindrome(listint_t **head)
 		list = list->next;
 
 	midpoint = list_len / 2;
+	--list_len;
 	list = *head;
 
 	for (i = 0; i < midpoint; i++)
 	{
-		if (list->n != list_index(*head, list_len - i -1))
+		if ((list + i)->n != (list + list_len - i)->n)
 			return (0);
-		list = list->next;
 	}
 	return (1);
 }
