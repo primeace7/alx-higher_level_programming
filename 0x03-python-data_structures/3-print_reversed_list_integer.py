cbd50665eeb0bacb6dd5e20d96f8cce818836386
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-13 20:18:56 +0000

    Fix task 3 test errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (len(my_list == 0):
+    if (len(my_list) == 0):
         print()
     for i in range(len(my_list)):
         out = my_list[len(my_list) - 1 - i]
