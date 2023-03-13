b1c35bd22c24c6c527d6b074f6558a2b66175441
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-13 15:26:43 +0000

    Fix tasks 0 and 3 test errors

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print('{}'.format(integer))
+    for i in reange(len(my_list)):
+        print('{}'.format(my_list[i]))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,3 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print('{}'.format(my_list[len(my_list) - i - 1]))
+    return
