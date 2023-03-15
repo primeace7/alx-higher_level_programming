8300def6fdc852f40a80921a9f496d011f68f88d
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-15 23:55:21 +0000

    Fix task 9 test error

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,7 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary is None:
         return None
+    new_dict = {}
     for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+        new_dict[key] = a_dictionary[key] * 2
+    return new_dict
