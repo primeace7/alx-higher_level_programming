81fd050768558cdd314f58b9588bd7c125b23a2a
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-16 02:15:44 +0000

    Remove backup files

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: x**2, ))
+    return list(map(lambda x:x**2, matrix))
 
 matrix = [
     [1, 2, 3],
--- 0x04-python-more_data_structures/12-roman_to_int.py~
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    
