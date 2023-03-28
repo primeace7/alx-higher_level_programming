98ed365daf067e7fbba2d47b721bbf2e736b16cf
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-28 15:17:15 +0000

    update changes in task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    names = dir('hidden_4')
+    names = dir('hidden_4.pyc')
     for name in names:
         if not name.startswith('__'):
             print('{}'.format(name))
