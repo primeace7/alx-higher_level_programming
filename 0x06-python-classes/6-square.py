26c532ee30173e24338a900b6472658f02cb5c43
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-28 22:28:10 +0000

    Change the return value of the position getter method to value of position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,7 +35,7 @@ class Square:
         Raises:
         TypeError: when position isn't a 2-element tuple of integers
         '''
-        return self.__property
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -88,7 +88,7 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                if self.__position[1] <= 0:
+                if self.__position[1] <= 1:
                     for i in range(self.__position[0]):
                         print(' ', end='')
                 for j in range(self.__size):
