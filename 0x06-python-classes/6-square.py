962bb1e19445f2bd828fef8bf67cb2547f3bcae4
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-29 11:19:53 +0000

    Change condition for printing the number of spaces in position before printing square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -87,11 +87,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for k in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                for k in range( self.__position[1]):
-                    print()
                 for i in range(self.__position[0]):
-                        print(' ', end='')
+                    print(' ', end='')
                 for j in range(self.__size):
                     print('#', end='')
                 print()
