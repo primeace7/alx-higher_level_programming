64f8888ce57aa2b3c8ba8bbe18040ab7e872c2ce
primeace7 <osaruonamenjeffery@gmail.com>
2023-06-12 10:33:50 +0000

    Add None input case to task 0 doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,6 @@
 This module is an implementation of an add function
 '''
 
-
 def add_integer(a, b=98):
     '''
     Add two integers and return the result
@@ -18,12 +17,16 @@ def add_integer(a, b=98):
     Raises:
     TypeError: a or b or both aren't integers
     '''
-    if a is None:
+    if a is None or (type(a) not in [int, float]):
         raise TypeError('a must be an integer')
-    elif type(a) not in [int, float]:
-        raise TypeError('a must be an integer')
-    elif b is None or type(b) not in [int, float]:
+    elif b is None or (type(b) not in [int, float]):
         raise TypeError('b must be an integer')
+
+    if a + 1 == a:
+        raise TypeError('a must be an integer')
+    elif b + 1 == b:
+        raise TypeError('b must be an integer')
+
     a = int(a)
     b = int(b)
 
