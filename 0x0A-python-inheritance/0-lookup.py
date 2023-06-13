05b8c75c1d7733d42a74696a149b180b83f9cdf5
primeace7 <osaruonamenjeffery@gmail.com>
2023-06-13 13:10:06 +0000

    Add module and function documentation for task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+'''
+define a function that returns all the names (functions, variables, attributes)
+defined in a particular object'''
 
 def lookup(obj):
+    '''
+    return list of names defined in an object
+
+    Args:
+    obj(object type): the object whose defined names should be returned
+
+    Returns:
+    list: a list of the defined names in obj
+    '''
+
     return dir(obj)
