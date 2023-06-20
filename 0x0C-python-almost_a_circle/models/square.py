d9731756f68e4f046483d483aed5f027479a8cbc
primeace7 <osaruonamenjeffery@gmail.com>
2023-06-20 20:35:40 +0000

    Add documentation to the init method of square module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,10 @@ from .rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        '''
+        init doc is above
+        all arg descriptions are in the class docstring
+        '''
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
