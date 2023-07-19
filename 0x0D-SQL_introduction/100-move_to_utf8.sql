0abe27b25b5dca125d53426b141475240ca849f2
primeace7 <osaruonamenjeffery@gmail.com>
2023-07-18 20:18:42 -0700

    Add tasks 18 and 19 files

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER DATABASE utf8mb4
+CHARACTER SET utf8mb4,
 ALTER TABLE hbtn_0c_0.first_table
 DEFAULT CHARACTER SET utf8mb4,
   MODIFY name varchar(256)
