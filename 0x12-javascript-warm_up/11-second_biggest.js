02f64f0ba9151f2af1e45adea6d43493172ec193
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-15 11:51:48 +0000

    Correct task 11 code in javascript warmup

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3) { console.log('0'); } else {
-  const argArray = process.argv.slice(2).toSorted();
+if (process.argv.length <= 3) { console.log(0); } else {
+    let argArray = process.argv.slice(2).toSorted();
+    argArray.forEach(elem => BigInt(elem));
   console.log(argArray[argArray.length - 2]);
 }
