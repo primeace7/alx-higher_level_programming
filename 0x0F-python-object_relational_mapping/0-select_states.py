423f05915a564d3cad354b4677bc5bfba8f4bb7e
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-18 11:30:08 +0000

    Fix task 0 requirement errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''
+a script that lists all states from the database hbtn_0e_0_usa
+Results must be sorted in ascending order by states.id
+'''
+
 
 import MySQLdb as DB
 db_connect = DB.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
@@ -6,8 +11,11 @@ cursor = db_connect.cursor()
 cursor.execute('SELECT * FROM hbtn_0e_0_usa.states ORDER BY id ASC')
 
 rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
 cursor.close()
 db_connect.close()
+
+
+
+if __name__ == '__main__':
+    for row in rows:
+        print(row)
