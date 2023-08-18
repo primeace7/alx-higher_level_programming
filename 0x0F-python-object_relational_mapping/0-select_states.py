14123c030334cfebf697a933e039aa70867d451e
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-18 11:36:01 +0000

    Fix task 0 code fail errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,8 @@ Results must be sorted in ascending order by states.id
 
 if __name__ == '__main__':
     import MySQLdb as DB
-    db_connect = DB.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
+    db_connect = DB.connect(host='localhost', port=3306, user='root',
+                            passwd='root', db='hbtn_0e_0_usa')
     cursor = db_connect.cursor()
     cursor.execute('SELECT * FROM hbtn_0e_0_usa.states ORDER BY id ASC')
 
