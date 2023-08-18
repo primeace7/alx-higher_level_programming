8c33cbb9eec2a81ef0272fc611a472433e72ebcb
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-18 10:09:48 +0000

    Fix task 0 requirement error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 import MySQLdb as DB
-db_connect = DB.connect(host='localhost', port=3306, user='root',\
-                        passwd='root', db='hbtn_0e_0_usa')
+db_connect = DB.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
 cursor = db_connect.cursor()
 cursor.execute('SELECT * FROM hbtn_0e_0_usa.states ORDER BY id ASC')
 
