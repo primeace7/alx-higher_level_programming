2e25e643199371a12e5a67f61799909de1721f4c
primeace7 <osaruonamenjeffery@gmail.com>
2023-09-01 22:02:54 +0000

    Add task 100 file

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+'''
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+
+    res = requests.get(url)
+
+    dates = []
+    commits = {}
+
+    res_json = res.json()
+
+    for result in res_json:
+        sha = result.get('sha')
+        author = result.get('commit').get('author').get('name')
+        date = result.get('commit').get('author').get('date')
+
+        dates.append(date)
+        commits[date] = sha + ': ' + author
+
+    dates.sort(reverse=True)
+
+    for i in range(10):
+        print(commits[dates[i]])
