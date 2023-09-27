25f26f14f08c8204dedd10b3812f18712390897b
primeace7 <osaruonamenjeffery@gmail.com>
2023-09-27 14:26:39 +0000

    Fix task 101 code errors

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,18 +4,30 @@
 
 const process = require('process');
 const request = require('request');
-
 const useURL = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(useURL, (error, response, body) => {
   if (error) { console.error(error); }
   const charactersList = JSON.parse(body).characters;
-  for (const character of charactersList) { getName(character); }
-});
+  getAge(charactersList);
+}
+);
 
-const getName = function (character) {
-  request(character, (error, response, body) => {
-    if (error) { console.error(error); }
-    console.log(JSON.parse(body).name);
-  });
+// fetches the character's creation date and converts it to time passed
+// since the epoch. Older characters will have smaller values
+const getAge = function (charactersList) {
+  const orderedCharacters = {};
+  for (const character of charactersList) {
+    request(character, (error, response, body) => {
+      if (error) { console.error(error); }
+      const name = JSON.parse(body).name;
+      const created = new Date(JSON.parse(body).created);
+      const age = created.getTime();
+      orderedCharacters[age] = name;
+    });
+  }
+  const sortedAge = Object.keys(orderedCharacters).toSorted();
+  for (let i = 0; i < orderedCharacters.length; i++) {
+    console.log(orderedCharacters[sortedAge[i]]);
+  }
 };
